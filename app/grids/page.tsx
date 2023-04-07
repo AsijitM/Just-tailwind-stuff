d4060d2f6fe012714870da23efe88cb4bbee7f6a
AsijitM <asijit00@gmail.com>
2023-04-07 22:43:46 +0530

    grid

--- app/grids/page.tsx
+++ app/grids/page.tsx
@@ -7,9 +7,12 @@ export default function Grid() {
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
           <div className="rounded-lg bg-sky-600 p-6 ">First element</div>
           <div className="rounded-lg bg-green-600 p-6 ">2nd element</div>
-          <div className="rounded-lg bg-purple-600 col-span-2 p-6 ">3rd element</div>
+          <div className="row-span-2 rounded-lg bg-purple-600 p-6 ">
+            3rd element
+          </div>
           <div className="rounded-lg bg-purple-600 p-6 ">4th element</div>
           <div className="rounded-lg bg-purple-600 p-6 ">5th element</div>
+
         </div>
       </div>
     </div>
