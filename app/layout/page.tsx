ee4d69a3ee58050fb9b16d28ec4dfea27dae2221
AsijitM <asijit00@gmail.com>
2023-04-07 22:57:36 +0530

    container

---
+++ app/layout/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Layout() {
+  return (
+    <div className="Parent h-screen bg-slate-900 text-white ">
+      <div className="container bg-red-500">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
